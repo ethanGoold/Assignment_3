1b83f4a407a6e17e0e20209030a022c0893934e8
ethanGoold <master.egoold@gmail.com>
2017-03-06 22:27:22 -0500

    Add files via upload

---
+++ GameModel.java
@@ -0,0 +1,104 @@
+import java.util.Random;
+
+
+public class GameModel { 
+
+	public static final int COLOR_0           = 0;
+    public static final int COLOR_1           = 1;
+    public static final int COLOR_2           = 2;
+    public static final int COLOR_3           = 3;
+    public static final int COLOR_4           = 4;
+    public static final int COLOR_5           = 5;
+    public static final int NUMBER_OF_COLORS  = 6;
+    private DotInfo[] model;
+    private int size;
+    private int steps = 0; 
+    private int currentSelectedColor; 
+
+    public GameModel (int size){
+    	Random generator = new Random();
+    	this.size = size;
+    	model = new DotInfo[size*size];
+    	for (int i = 0; i< model.length; i++){
+    		model[i] = new DotInfo(i/size, i%size. generator.nextInt(6));
+
+    	}
+
+    }
+
+    public void reset(){
+    	model = new DotInfo[size*size];
+    	for (int i = 0; i< model.length; i++){
+    		model[i] = new DotInfo(i/size, i%size. generator.nextInt(6));
+    	}
+    	steps = 0;
+    }
+
+    public int getSize(){
+    	return size;
+
+    }
+
+    public int getColor(int i, int j){
+    	return model[(size*i) + j].getColor();
+
+    }
+
+    public boolean isCaptured(int i, int j){
+    	return model[(size*i) + j].isCaptured();
+
+    }
+
+    public void capture(int i, int j){
+    	model[(size*i) + j].setCaptured();
+
+
+    }
+
+    public int getNumberOfSteps(){
+    	return steps; 
+
+
+    }
+
+    public void setCurrentSelectedColor(int val){
+    	currentSelectedColor = val;
+
+
+    }
+
+    public int getCurrentSelectedColor(){
+    	return currentSelectedColor;
+
+
+    }
+
+    public DotInfo get(int i, int j){
+    	return model[size*i + j];
+
+    }
+
+    public void step(){
+    	steps++;
+
+
+    }
+
+    public boolean isFinished(){
+    	private boolean result = true;
+    	for (int i = 0; i<model.length; i++){
+    		if ()
+    	}
+
+
+    }
+
+    public String toString(){
+    	StringBuilder string = new StringBuilder();
+    	for (int i = 0; i<model.length; i++){
+    		string.append("The dot at row" + i/size + "and column" + i%size + "is color " + method[i].getColor());
+    	return string.toString();
+    	}
+
+    }
+}
